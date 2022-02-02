2ca65999470d19a5a3b4502b25bf682c805739c8
CarolynnFleming <cfcatpink@gmail.com>
2022-02-01 19:50:27 -0500

    finished beverage state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 function App() {
   const [dishId, theDishId] = useState(1);
-  const [beverageId, theBeverageId]
+  const [beverageId, theBeverageId] = useState(1);
   return (
     <div className="App">
       

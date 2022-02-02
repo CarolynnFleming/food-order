089b0825200c97761c5cd06801821363cb9bebf6
CarolynnFleming <cfcatpink@gmail.com>
2022-02-01 20:03:26 -0500

    beverage imgae src added

--- src/DishImages.js
+++ src/DishImages.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function DishImages({ dishId, beverageId, sideId }) {
   return <div className='image-menu'>
-      <img className="yum" src={`dish-${dishId}.jpeg`} />
+      <img className="yum" src={`dish-${dishId}.jpeg`}/>
+      <img className="yum" src={`beverage-${beverageId}.jpeg`}/>
   </div>;
 }

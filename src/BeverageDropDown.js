ff379abb70d17197181d10a062e41cbd97529fb7
CarolynnFleming <cfcatpink@gmail.com>
2022-02-04 12:52:31 -0500

    handlchange function

--- src/BeverageDropDown.js
+++ src/BeverageDropDown.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
 export default function BeverageDropDown({ setBeverageId }) {
+    function handleChange(e) {
+        setBeverageId(e.target.value);
+      }
   return <div></div>;
 }

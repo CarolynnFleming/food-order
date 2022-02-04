2f966901c302ec414c5ef8cb7580614299e07080
CarolynnFleming <cfcatpink@gmail.com>
2022-02-04 13:09:36 -0500

    handleSubmit

--- src/InstructionForm.js
+++ src/InstructionForm.js
@@ -3,5 +3,13 @@ import { useState } from 'react';
 
 export default function InstructionForm({ setBasicInstuctions, basicInstructions }) {
   const [instructionForm, setInstructiomForm] = useState('');
+
+  function handleSubmit(e){
+      e.preventDefault();
+
+      setBasicInstuctions([...basicInstructions, instructionForm]);
+
+      setInstructiomForm('');
+  }
   return <div></div>;
 }

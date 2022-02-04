11760dca348684f8d7124405348802e5075114d2
CarolynnFleming <cfcatpink@gmail.com>
2022-02-04 12:48:28 -0500

    changed state

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import { useState } from 'react';
 import DishImages from './DishImages.js';
 import NameInput from './NameInput.js';
 import InstructionList from './InstructionList.js';
+import DishDropDown from './DishDropDown.js';
 
 
 function App() {
-  const [dishId, theDishId] = useState(1);
-  const [beverageId, theBeverageId] = useState(1);
-  const [sideDishId, theSideDishId] = useState(1);
-  const [orderName, theOrderName] = useState('loyal customer');
-  const [basicInstructions, theBasicInstructions] = useState(['Make it GREAT!!!', 'Make it  YUMMY!!!']);
+  const [dishId, setDishId] = useState(1);
+  const [beverageId, setBeverageId] = useState(1);
+  const [sideDishId, setSideDishId] = useState(1);
+  const [orderName, setOrderName] = useState('loyal customer');
+  const [basicInstructions, setBasicInstructions] = useState(['Make it GREAT!!!', 'Make it  YUMMY!!!']);
   return (
     <div className="App">
       <h1>Order for {orderName}</h1>
       <DishImages dishId={dishId} beverageId={beverageId} sideDishId={sideDishId}/>
-      <NameInput theOrderName={theOrderName}/>
+      <NameInput setOrderName={setOrderName}/>
       <InstructionList basicInstructions={basicInstructions}/>
+      <section>
+        <DishDropDown setDishId={setDishId}/>
+      </section>
     </div>
   );
 }

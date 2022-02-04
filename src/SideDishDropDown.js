384610611069ced038002f9dc9a1ac14f25e5b0b
CarolynnFleming <cfcatpink@gmail.com>
2022-02-04 12:58:52 -0500

    handlechange function

--- src/SideDishDropDown.js
+++ src/SideDishDropDown.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
 export default function SideDishDropDown({ setSideDishId }) {
+    function handleChange(e) {
+        setSideDishId(e.target.value);
+      }
   return <div></div>;
 }

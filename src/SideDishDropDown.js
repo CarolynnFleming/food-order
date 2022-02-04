1978111c3cf90c3ba4a086b198363312cd02fac8
CarolynnFleming <cfcatpink@gmail.com>
2022-02-04 13:00:46 -0500

    sidedish dropdown done

--- src/SideDishDropDown.js
+++ src/SideDishDropDown.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 export default function SideDishDropDown({ setSideDishId }) {
-    function handleChange(e) {
-        setSideDishId(e.target.value);
-      }
-  return <div></div>;
+  function handleChange(e) {
+    setSideDishId(e.target.value);
+  }
+  return (<div>
+ Choose a Side:
+    <select onChange={handleChange}>
+      <option value="1">Fries</option>
+      <option value="2">Fried Pickles</option>
+      <option value="3">Cucumber Salade</option>
+    </select>
+  </div>);
 }
